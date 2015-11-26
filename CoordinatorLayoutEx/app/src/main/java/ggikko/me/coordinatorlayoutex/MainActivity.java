097c24f8a39ce33ee2c0fb6795bed0c54df3622a
ggikko <ggikko@hotmail.com>
2015-11-27 02:41:39 +0900

    test

--- CoordinatorLayoutEx/app/src/main/java/ggikko/me/coordinatorlayoutex/MainActivity.java
+++ CoordinatorLayoutEx/app/src/main/java/ggikko/me/coordinatorlayoutex/MainActivity.java
@@ -1,13 +1,21 @@
 package ggikko.me.coordinatorlayoutex;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        NavigationView navView = (NavigationView) findViewById(R.id.navigation_view);
+
     }
 }
